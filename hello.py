fb2a2c0ebd5c62227ae31a36f006bcfb7ca41589
Lucas Eduardo <lucaseduardo168@gmail.com>
2025-05-12 07:56:24 -0300

    hello world

---
+++ hello.py
@@ -0,0 +1,13 @@
+#py -3 -m venv .venv
+#.venv\Scripts\activate
+# pip install flask
+# python -m flask --app <arquivo> run --debug
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'Hello, World!'
